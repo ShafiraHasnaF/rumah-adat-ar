49ef992f5f406e47866156ef2488724d1f6d1be0
Shafira Hasna Fauzi <shafirahasna32@gmail.com>
2026-05-20 23:27:59 +0700

    updating UI

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
     splashScreen();
+
+    //btn filter di homepage
+    const btnFilterPulau = document.querySelectorAll(".filter-btn");
+    btnFilterPulau.forEach(button => {
+        button.addEventListener("click", () => {
+            btnFilterPulau.forEach(btn => {
+                btn.classList.remove(
+                    "bg-white",
+                    "text-[#4E2012]",
+                    "rounded-t-2xl",
+                    "px-6"
+                );
+                btn.classList.add(
+                    "text-white",
+                    "border-b-4",
+                    "border-white"
+                );
+            });
+            button.classList.remove(
+                "text-white",
+                "border-b-4",
+                "border-white"
+            );
+            button.classList.add(
+                "bg-white",
+                "text-[#4E2012]",
+                "rounded-t-2xl",
+                "px-6"
+            );
+        });
+    });
 })
 
 function splashScreen() {
     const splashScreen = document.getElementById("splash-screen");
-    const homepage = document.getElementById("homepage");
+    const guidepage = document.getElementById("guidepage");
     setTimeout(() => {
         splashScreen.classList.remove("opacity-100");
         splashScreen.classList.add("opacity-0");
         setTimeout(() => {
             splashScreen.style.display = "none";
-            homepage.classList.remove("opacity-0");
-            homepage.classList.add("opacity-100");
+            guidepage.classList.remove("opacity-0");
+            guidepage.classList.add("opacity-100");
         }, 500);
     }, 3000);
 }
